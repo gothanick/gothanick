ee4f8021635b256968e19a90856ea39914db4a12
gothanick <augeumauge@protonmail.com>
2023-12-19 21:12:46 +0100

    P tag commit

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,8 @@ export default function Page() {
         <div
           className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
         />
+
+        <p>Dette er et previes!!!!!!!!!!!!</p>
         <p
          className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
         >            
